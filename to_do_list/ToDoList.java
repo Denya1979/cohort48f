7579d54c481c79eda20dbb3d90e93b3e2d407065
Denis <Cemencemenovich1937@outlook.com>
2024-08-28 21:15:10 +0200

    Update

---
+++ to_do_list/ToDoList.java
@@ -0,0 +1,32 @@
+package homework_30.to_do_list;
+
+public enum ToDoList {
+    ADD_TASK(1, "add task"),VIEW_TASKS(2, "view all tasks"),
+    DELETE_TASK(3, "delete task (by number)"), EXIT(4, "exit the menu");
+
+    // field
+   private  int optionNumber;
+    private  String description;
+
+    ToDoList(int optionNumber, String description) {
+        this.optionNumber = optionNumber;
+        this.description = description;
+    }
+
+    public int getOptionNumber() {
+        return optionNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ToDoList{");
+        sb.append("optionNumber=").append(optionNumber);
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
