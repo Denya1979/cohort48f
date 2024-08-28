93e5577ddbae0e4ece5d4473bf61cd9b4d9bcdb8
Denis <Cemencemenovich1937@outlook.com>
2024-08-28 21:15:27 +0200

    Update

---
+++ to_do_list/ToDoListAppl.java
@@ -0,0 +1,29 @@
+package homework_30.to_do_list;
+
+import java.util.Scanner;
+
+public class ToDoListAppl {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("To-Do List");
+        ToDoList[] content =  ToDoList.values();
+        for (ToDoList toDoList : content) {
+            System.out.println(toDoList);
+        }
+        boolean flag = true;
+        while (flag){
+            System.out.println("Input your choice");
+            int optionNumber = scanner.nextInt();
+            switch (optionNumber){
+                case 1 -> System.out.println("Your choice is: " + content[optionNumber - 1].toString());
+                case 2 -> System.out.println("Your choice is: " + content[optionNumber - 1].toString());
+                case 3 -> System.out.println("Your choice is: " + content[optionNumber - 1].toString());
+                case 4 -> System.out.println("Your choice is: " + content[optionNumber - 1].toString());
+                case 5 -> flag = false;
+                default -> System.out.println("Wrong choice");
+            }
+
+        }
+    }
+}
